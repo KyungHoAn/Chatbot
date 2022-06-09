9abd92e7f30829d59688f2a7bd2bc3ea6a7aa69b
akh98 <akh1216@gmail.com>
2022-06-09 12:00:22 +0900

    KH 2022/06/09 commit

---
+++ ChatBot/src/main/java/com/example/demo/api/naver/ChatBotPracApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.api.naver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotPracApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application 연결 blog2");
+		SpringApplication.run(ChatBotPracApplication.class, args);
+	}
+
+}
